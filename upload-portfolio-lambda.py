5ef3d4be6713d79a3864979a414f21189fd8b457
James Small <github@nethole.com>
2017-09-07 17:23:11 -0400

    write to explicitly named buckets

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -3,14 +3,16 @@ from botocore.client import Config
 import StringIO
 import zipfile
 
+s3 = boto3.resource('s3')
+
 portfolio_bucket = s3.Bucket('stormageddon')
 build_bucket = s3.Bucket('resumestormageddondarkness.com')
 
-portfolio_zip = StringIO.stringIO()
-build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfolio.zip',portfolio_zip)
 
 with zipfile.ZipFile(portfolio_zip) as myzip:
   for nm in myzip.namelist():
     obj = myzip.open(nm)
     portfolio_bucket.upload_fileobj(obj,nm)
-    portfolio_bucket.Object(nm).Acl().pub(ACL='public-read')
+    portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
