ba627e58f592e9a161d847c17999d895b7054a21
James Small <github@nethole.com>
2017-09-07 17:26:58 -0400

    mimetype guess

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,6 +2,7 @@ import boto3
 from botocore.client import Config
 import StringIO
 import zipfile
+import mimetypes
 
 s3 = boto3.resource('s3')
 
@@ -14,5 +15,6 @@ build_bucket.download_fileobj('portfolio.zip',portfolio_zip)
 with zipfile.ZipFile(portfolio_zip) as myzip:
   for nm in myzip.namelist():
     obj = myzip.open(nm)
-    portfolio_bucket.upload_fileobj(obj,nm)
+    portfolio_bucket.upload_fileobj(obj,nm,
+      ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
     portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
