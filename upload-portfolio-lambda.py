c5d5e6a74da2e2e1d938e6e03581aef8f723b016
Git Lap <gitlap@nethole.com>
2017-09-26 09:38:32 -0400

    with codepipeline success

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,12 +4,6 @@ import StringIO
 import zipfile
 import mimetypes
 
-import boto3
-from botocore.client import Config
-import StringIO
-import zipfile
-import mimetypes
-
 
 
 def lambda_handler(event, context):
@@ -17,13 +11,23 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:473673896446:randomclass')
 
+    location = {
+        "bucketName": "resumestormageddondarkness.com"
+        "objectKey": "portfolio.zip"
+    }
     try:
-    
+        job = event.get("CodePipeline.job")
+        if job:
+            for artifact in job["data"]["inputArtifacts"]
+                if artifact["name"]="MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+        
+        print "Building portfolio from " + str(location)
         portfolio_bucket = s3.Bucket('stormageddon')
-        build_bucket = s3.Bucket('resumestormageddondarkness.com')
+        build_bucket = s3.Bucket(location["bucketName]")
         
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfolio.zip',portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"],portfolio_zip)
     
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -35,6 +39,10 @@ def lambda_handler(event, context):
                 
         print "job done"
         topic.publish(Subject="test 2",Message="lambda message succeed")
+        if job:
+            codepipeline = boto3.client("codepipeline")
+            codepipeline.put_job_success_result(jobId=job["id"])
+            
     except:
         topic.publish(Subject="test 2",Message="lamba message fail")
 
