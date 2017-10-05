dd4c2c34f7aa020e93d92030f64234c6ad9cba1a
James Small <github@nethole.com>
2017-10-05 09:01:59 -0400

    correcting binary mimetype

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -21,29 +21,28 @@ def lambda_handler(event, context):
             for artifact in job["data"]["inputArtifacts"]:
                 if artifact["name"] == "MyAppBuild":
                     location = artifact["location"]["s3Location"]
-        
+
         print "Building portfolio from " + str(location)
         portfolio_bucket = s3.Bucket('stormageddon')
         build_bucket = s3.Bucket(location["bucketName"])
-# line 27 error questionmark    
-    
+# line 27 error questionmark
+
         portfolio_zip = StringIO.StringIO()
         build_bucket.download_fileobj(location["objectKey"],portfolio_zip)
-    
+
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj,nm)
-    #    ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
     
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-                
+
         print "job done"
         topic.publish(Subject="Portfolio deployed",Message="lambda portfolio deployed succeed")
         if job:
             codepipeline = boto3.client("codepipeline")
             codepipeline.put_job_success_result(jobId=job["id"])
-            
+
     except:
         topic.publish(Subject="Portfolio not deployed ",Message="lamba message fail")
 
