87e22ead5c9748b7b08a96674decc1a80cf88ec7
James Small <github@nethole.com>
2017-10-05 10:33:22 -0400

    components

--- js/example-work.js
+++ js/example-work.js
@@ -6,7 +6,7 @@ class ExampleWork extends React.Component {
       <section className="section section--alignCentered section--description">
       { this.props.work.map( (example,idx) => {
         return (
-          <ExampleWorkBubble />
+          <ExampleWorkBubble example={example} key={idx} />
         )
       })}
 
@@ -21,15 +21,16 @@ class ExampleWork extends React.Component {
 
 class ExampleWorkBubble extends React.Component {
   render() {
+    let example = this.props.example;
     return (
       <div className="section__exampleWrapper">
         <div className="section__example">
-          <img alt="example screenshot of a project involving code"
+          <img alt={ example.image.desc }
              className="section__exampleImage"
-             src="images/example1.png"/>
+             src={ example.image.src }/>
           <dl className="color--cloud">
             <dt className="section__exampleTitle section__text--centered">
-               Work ExampleWork
+               { example.title }
             </dt>
             <dd></dd>
           </dl>
