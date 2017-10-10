5cae245ea11c198aec85e7e576c78f23e9dde31f
James Small <github@nethole.com>
2017-10-10 13:59:10 -0400

    adding wait for create and delete into script

--- CF/adduser.sh
+++ CF/adduser.sh
@@ -6,8 +6,16 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
+STACKNAME=userforclass
 
 
-aws cloudformation  create-stack --stack-name userforclass --capabilities CAPABILITY_IAM \
+
+
+aws cloudformation delete-stack --stack-name $STACKNAME
+aws cloudformation wait stack-delete-complete --stack-name $STACKNAME
+
+aws cloudformation  create-stack --stack-name $STACKNAME --capabilities CAPABILITY_NAMED_IAM \
 --template-body file://./adduser.yml \
 --parameters ParameterKey=Password,ParameterValue=$2 ParameterKey=EnteredUserName,ParameterValue=$1
+
+aws cloudformation wait stack-create-complete --stack-name $STACKNAME
