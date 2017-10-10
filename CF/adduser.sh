eaa8301fb6833d5f0090646fab7db9f92764136e
James Small <github@nethole.com>
2017-10-10 13:45:07 -0400

    formalizing

--- CF/adduser.sh
+++ CF/adduser.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo Script name: $0
 echo $# arguments
-if [$# -ne 1];
-    then echo "Usage $0: UserName Password"
+if [ "$#" -ne 2 ]; then
+    echo "Usage $0: UserName Password"
     exit 1
 fi
 
 
 
-aws cloudformation  create-stack --stack-name userforclass \
---template-body file:./adduser.yml \
-ParameterKey=Password,ParameterValue=$2 ParameterKey=Name,ParameterValue=$1
+aws cloudformation  create-stack --stack-name userforclass --capabilities CAPABILITY_IAM \
+--template-body file://./adduser.yml \
+--parameters ParameterKey=Password,ParameterValue=$2 ParameterKey=EnteredUserName,ParameterValue=$1
